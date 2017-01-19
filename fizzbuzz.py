7bc172c3825b40e141ab9282ca653d42710e1783
GDP254 <stvnkrs@gmail.com>
2017-01-19 20:30:38 +0300

    Adding narrative functionality to fizzbuzz lab

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,19 @@
 
 def fizz_buzz(fbval):
     if type(fbval) is int:
+        print "Check if value has a remainder when divided by 3"
         three_mod = fbval % 3;
+        print "Check if value has a remainder when divided by 5"
         five_mod = fbval % 5;
+        
+        print "if division by both 3 and 5 does not provide a remainder:"
+        print " FizzBuzz"
+        print "else if both operations provide remainders:"
+        print " return value"
+        print "else if only division by 3 does not provide a remainder:"
+        print " Fizz"
+        print "else (only division by 5 does not provide a remainder):"
+        print " Buzz"
         
         if three_mod == 0 and five_mod == 0:
             #divisible by both three and five
